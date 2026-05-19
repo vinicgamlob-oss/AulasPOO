d4d8818c0eecf565d797bc2bd010cfc2743d2c71
Vinicius Gama Bittencourt Lobo <vinicgamlob@gmail.com>
2026-05-19 15:59:33 -0300

    Update

---
+++ Calculadora/src/calculadora/Operacoes.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Vinicius
+ */
+public class Operacoes {
+    
+    public double soma(double a ,double b ){
+        double Soma = a+b;
+        return Soma; 
+    }
+    
+     public double Sub(double a ,double b ){
+        double Sub = a-b;
+        return Sub; 
+    }
+     
+     public double multi(double a , double b){
+         double Multi = a*b;
+         return Multi;
+     }
+     
+     public double div(double a , double b){
+        double Div = a/b;
+         return Div;
+     }
+    
+}
